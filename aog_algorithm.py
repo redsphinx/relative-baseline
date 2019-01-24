05941f14cf979488cc55caee6c00674be1e03547
u680172 <g.ras@donders.ru.nl>
2019-01-24 14:57:25 +0100

    fixed algorithm

--- aog_algorithm.py
+++ aog_algorithm.py
@@ -2,8 +2,10 @@
 https://arxiv.org/pdf/1711.05847.pdf
 '''
 
+by_first = True
+
 # init
-o_0, o_1, o_2 = ['o', 0, 0], ['o', 0, 1], ['o', 0, 2]
+n = 3
 or_nodes = [['o', 0, 0], ['o', 0, 1], ['o', 0, 2]]
 v = [['o', 0, 0], ['o', 0, 1], ['o', 0, 2]]
 q = [['o', 0, 0], ['o', 0, 1], ['o', 0, 2]]
@@ -11,6 +13,8 @@ e = []
 t_nodes = []
 and_nodes = []
 
+if by_first:
+    q.reverse()
 
 while(len(q) is not 0):
     v_i_j = q.pop()
@@ -25,10 +29,20 @@ while(len(q) is not 0):
         e_v_t = [v_i_j, t_i_j]
         e.append(e_v_t)
         # ii) create AND-nodes for valid splits 0<=m<k
-        for m in range(k-1):
+        for m in range(k):
             a_i_j_m = ['a', v_i_j[1], v_i_j[2], m]
             and_nodes.append(a_i_j_m)
-            q.append(a_i_j_m)
+            e_v_a = [v_i_j, a_i_j_m]
+            if e_v_a not in e:
+                if [a_i_j_m, v_i_j] not in e:
+                    e.append(e_v_a)
+            if a_i_j_m not in v:
+                v.append(a_i_j_m)
+                if by_first:
+                    q.insert(0, a_i_j_m)
+                else:
+                    q.append(a_i_j_m)
+
 
     # if v_i_j is an AND-node with split m
     elif v_i_j[0] == 'a':
@@ -44,14 +58,22 @@ while(len(q) is not 0):
         e.append(e_v_o)
         if o_i_im not in v:
             v.append(o_i_im)
-            q.append(o_i_im)
+            if by_first:
+                q.insert(0, o_i_im)
+            else:
+                q.append(o_i_im)
         if o_im1_j not in v:
             v.append(o_im1_j)
-            q.append(o_im1_j)
+            if by_first:
+                q.insert(0, o_im1_j)
+            else:
+                q.append(o_im1_j)
 
 
-print('vertices: ', v)
-print('or-nodes: ', or_nodes)
-print('t-nodes: ', t_nodes)
-print('and-nodes: ', and_nodes)
-print('edges: ', e)
+print('vertices: ')
+for i in range(len(v)):
+    print(v[i])
+
+print('edges: ')
+for i in range(len(e)):
+    print(e[i])
