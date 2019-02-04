e2671b65eae809127763b86aeb89efb1163bb5cc
redsphinx <gabi.ras@hotmail.com>
2019-02-04 09:58:06 +0100

    fixed bug in data extraction

--- extract_omg.py
+++ extract_omg.py
@@ -1,6 +1,7 @@
 import os
 import skvideo.io
 from PIL import Image
+from tqdm import tqdm
 
 
 def video_to_frames(dataset, path, extract='participant', format='jpg', num_frames=None, dims=None):
@@ -31,38 +32,43 @@ def video_to_frames(dataset, path, extract='participant', format='jpg', num_fram
         # h x w
         dims = (cut[1]-cut[0], 720)
 
-
     # for mp4 in path
     all_videos = os.listdir(p)
 
     for mp4 in all_videos:
         # create jpg folder
         jpg_folder = save_location + '/' + mp4.split('.mp4')[0]
+
         if not os.path.exists(jpg_folder):
             os.mkdir(jpg_folder)
+            print('%s created' % jpg_folder)
 
-        # read in video, shape: (8675, 720, 2560, 3)
-        if num_frames is None:
-            num_frames = 0
-        mp4_arr = skvideo.io.vread(p + '/' + mp4, num_frames=num_frames)
+            # read in video, shape: (8675, 720, 2560, 3)
+            if num_frames is None:
+                num_frames = 0
 
-        # for each number of frames, in video
-        num_frames = mp4_arr.shape[0]
+            print('extracting %s ...' % mp4)
+            mp4_arr = skvideo.io.vread(p + '/' + mp4, num_frames=num_frames)
 
-        for i in range(num_frames):
-            frame = mp4_arr[i]
-            name_img = jpg_folder + '/' +str(i) + '.' + format
+            # for each number of frames, in video
+            num_frames = mp4_arr.shape[0]
 
-            # save frame as format in size dims
-            frame_img = Image.fromarray(frame)
-            # frame_img.size = (2560, 720)
-            frame_img = frame_img.crop((cut[0], 0, cut[1], 720)) # left, upper, right, and lower
-            frame_img = frame_img.resize((dims[0], dims[1]))
-            frame_img.save(name_img, mode='RGB')
+            for i in tqdm(range(num_frames)):
+                frame = mp4_arr[i]
+                name_img = jpg_folder + '/' +str(i) + '.' + format
+
+                # save frame as format in size dims
+                frame_img = Image.fromarray(frame)
+                # frame_img.size = (2560, 720)
+                frame_img = frame_img.crop((cut[0], 0, cut[1], 720)) # left, upper, right, and lower
+                frame_img = frame_img.resize((dims[0], dims[1]))
+                frame_img.save(name_img, mode='RGB')
+
+            num_frames = None
 
 
 path_to_data = '/scratch/users/gabras/data/omg_empathy'
 
-video_to_frames(dataset='Validation', path=path_to_data, dims=(640, 360))
+# video_to_frames(dataset='Validation', path=path_to_data, dims=(640, 360))
 video_to_frames(dataset='Training', path=path_to_data, dims=(640, 360))
 
