c1001488e11244cfafa9cca4a8b08f9fa77e736d
u680172 <g.ras@donders.ru.nl>
2019-03-19 14:05:23 +0100

    added optimizer

--- omg_emotion/setup.py
+++ omg_emotion/setup.py
@@ -1,11 +1,9 @@
 from .settings import ProjectVariable
 from torchvision.models import resnet18
-
+from torch.optim.adam import Adam
 
 
 def get_model(project_variable):
-    project_variable = ProjectVariable()
-
     if project_variable.model_number == 0:
         model = resnet18(pretrained=True)
     else:
@@ -15,7 +13,12 @@ def get_model(project_variable):
 
 
 def get_optimizer(project_variable):
-    project_variable = ProjectVariable()
+    if project_variable.optimizer == 'adam':
+        optimizer = Adam(lr=project_variable.learning_rate)
+    else:
+        optimizer = None
+
+    return optimizer
 
 
 def get_device(project_variable):
