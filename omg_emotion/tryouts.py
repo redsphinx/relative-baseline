8c382afb3351bd0d3b31120d7bebe7fd497f9bae
u680172 <g.ras@donders.ru.nl>
2019-03-27 15:25:03 +0100

    small

--- omg_emotion/tryouts.py
+++ omg_emotion/tryouts.py
@@ -1,21 +1,7 @@
-from torchvision.models import resnet18
-from torch.optim.adam import Adam
-import torch
-from torch import nn
 
 # https://stackoverflow.com/questions/51801648/how-to-apply-layer-wise-learning-rate-in-pytorch
 
+print('hello there')
 
-batch_size = 5
-nb_classes = 2
-in_features = 10
+print(4)
 
-model = nn.Linear(in_features, nb_classes)
-criterion = nn.CrossEntropyLoss()
-
-x = torch.randn(batch_size, in_features)
-target = torch.empty(batch_size, dtype=torch.long).random_(nb_classes)
-
-output = model(x)
-loss = criterion(output, target)
-loss.backward()
