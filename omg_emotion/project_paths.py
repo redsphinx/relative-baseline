49742fdc5ef0609627143eb96377c4450ccfb666
u680172 <g.ras@donders.ru.nl>
2019-03-29 11:33:19 +0100

    lovelace not lovalace

--- omg_emotion/project_paths.py
+++ omg_emotion/project_paths.py
@@ -1,7 +1,7 @@
 import os
 
-# server = 'lovalace'
-server = 'schmidhuber'
+server = 'lovelace'
+# server = 'schmidhuber'
 
 if server == 'schmidhuber':
     server_path = '/scratch/users/gabras/data'
@@ -19,3 +19,8 @@ omg_emotion_jpg = 'jpg_full_body_background_1280_720'
 models = os.path.join(saving_data, 'models')
 
 writer_path = os.path.join(saving_data, 'tensorboardX')
+
+affect_net_base = '/home/gabras/scratch/AffectNet/manually_annotated'
+affectnet_jpgs = '/home/gabras/scratch/AffectNet/manually_annotated/all_images'
+affectnet_labels_train = '/home/gabras/scratch/AffectNet/manually_annotated/training.csv'
+affectnet_labels_val = '/home/gabras/scratch/AffectNet/manually_annotated/validation.csv'
