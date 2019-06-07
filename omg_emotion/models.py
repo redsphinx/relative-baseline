5afd96a1af27f11fec9c684295a6c5c23da6a5e2
u680172 <g.ras@donders.ru.nl>
2019-06-07 12:48:31 +0200

    implementation of convTTN3d complete, needs debugging

--- omg_emotion/models.py
+++ omg_emotion/models.py
@@ -93,42 +93,38 @@ class LeNet5_3d(torch.nn.Module):
         return x
 
 
-# TODO: fix the matrix/vector multiplications
-# def make_affine_matrix(out_channels, scale, rotate, translate_x, translate_y, to_tensor=True):
-#     """
-#     :param scale: list of scale ints
-#     :param rotate: list of rotation ints
-#     :param translate_x: list of translation x ints
-#     :param translate_y: list of translation y ints
-#     :param to_tensor: boolean, defualt to True
-#     :return: if to_tensor, tensor, else numpy matrix
-#     """
-#
-#     matrix = np.zeros(shape=(out_channels, 2, 3), dtype=np.float)
-#
-#     # https://en.wikipedia.org/wiki/Transformation_matrix
-#     matrix[:][0][0] = scale * np.cos(rotate)
-#     matrix[:][1][0] = -scale * np.sin(rotate)
-#     matrix[:][2][0] = translate_x * scale * np.cos(rotate) - translate_y * scale * np.sin(rotate) + np.transpose(translate_x) # t'_x
-#     matrix[:][0][1] = scale * np.sin(rotate)
-#     matrix[:][1][1] = -scale * np.cos(rotate)
-#     matrix[:][2][1] = translate_x * scale * np.sin(rotate) + translate_y * scale * np.cos(rotate) + np.transpose(translate_y) # t'_y
-#
-#     if to_tensor:
-#         matrix = torch.from_numpy(matrix)
-#
-#     return matrix
-
-
 def make_affine_matrix(scale, rotate, translate_x, translate_y):
-    matrix = []
-    matrix = torch.tensor(matrix)
+
+    assert scale.shape == rotate.shape == translate_x.shape == translate_y.shape
+
+    matrix = torch.zeros((scale.shape[0], scale.shape[1], 2, 3))
+
+    # first transform is the identity
+    matrix[0] = torch.eye(3)[:2]
+
+    # i = number of filters
+    # j = number of transformations
+    for i in range(1, scale.shape[0]):
+        for j in range(scale.shape[1]):
+            matrix[i][j][0][0] = scale[i][j] * torch.cos(rotate[i][j])
+            matrix[i][j][0][1] = -scale[i][j] * torch.sin(rotate[i][j])
+            matrix[i][j][0][2] = translate_x[i][j] * scale[i][j] * torch.cos(rotate[i][j]) - translate_y[i][j] * \
+                                 scale[i][j] * torch.sin(rotate[i][j]) + torch.transpose(translate_x[i][j])
+            matrix[i][j][1][0] = scale[i][j] * torch.sin(rotate[i][j])
+            matrix[i][j][1][1] = -scale[i][j] * torch.cos(rotate[i][j])
+            matrix[i][j][1][2] = translate_x[i][j] * scale[i][j] * torch.sin(rotate[i][j]) + translate_y[i][j] * \
+                                 scale[i][j] * torch.cos(rotate[i][j]) + torch.transpose(translate_y[i][j])
+
     return matrix
 
 
 
 class ConvTTN3d(conv._ConvNd):
+    # for now: assume different transformations for each slice on the time axis
 
+    # number of filters = out_channels
+    # number of transformations needed = kernel_size[0], where the first transformation is the identity 
+    # 
     def __init__(self, in_channels, out_channels, kernel_size,
                  stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'):
         kernel_size = _triple(kernel_size)
@@ -140,110 +136,29 @@ class ConvTTN3d(conv._ConvNd):
             in_channels, out_channels, kernel_size, stride, padding, dilation,
             False, _triple(0), groups, bias, padding_mode)
 
+        print(kernel_size.shape)
+
         # ------
         # affine parameters
-        self.scale = torch.nn.init.normal(torch.nn.Parameter(torch.zeros(1)))
-        # TODO: angle in radians?
-        self.rotate = torch.nn.init.normal(torch.nn.Parameter(torch.zeros(1)))
-        self.translate_x = torch.nn.init.normal(torch.nn.Parameter(torch.zeros(1)))
-        self.translate_y = torch.nn.init.normal(torch.nn.Parameter(torch.zeros(1)))
+        self.scale = torch.nn.init.normal(torch.nn.Parameter(torch.zeros((out_channels, kernel_size[0]))))
+        self.rotate = torch.nn.init.normal(torch.nn.Parameter(torch.zeros((out_channels, kernel_size[0]))))
+        self.translate_x = torch.nn.init.normal(torch.nn.Parameter(torch.zeros((out_channels, kernel_size[0]))))
+        self.translate_y = torch.nn.init.normal(torch.nn.Parameter(torch.zeros((out_channels, kernel_size[0]))))
         # affine transformation matrix
-        # TODO: assume different transformations for each timepoint on the time axis
         self.theta = make_affine_matrix(self.scale, self.rotate, self.translate_x, self.translate_y)
-        self.grid = F.affine_grid(self.theta, )
-
-
+        self.grid = F.affine_grid(self.theta, torch.Size([out_channels, kernel_size[0], kernel_size[1], kernel_size[2]]))
+        print(self.grid.shape)
         # ------
 
 # example of how grid is used: https://discuss.pytorch.org/t/affine-transformation-matrix-paramters-conversion/19522
-
+# assuming transfer learning scenario
     def forward(self, input, kernels2d):
 
         # ------
+        self.weight = self.transform(kernels2d, self.grid)
 
-
-
-        # ------
-
-
-
-        if self.padding_mode == 'circular':
-            expanded_padding = ((self.padding[2] + 1) // 2, self.padding[2] // 2,
-                                (self.padding[1] + 1) // 2, self.padding[1] // 2,
-                                (self.padding[0] + 1) // 2, self.padding[0] // 2)
-            return F.conv3d(F.pad(input, expanded_padding, mode='circular'),
-                            self.weight, self.bias, self.stride, _triple(0),
-                            self.dilation, self.groups)
-
-
-
+        self.weight.requires_grad = False
         return F.conv3d(input, self.weight, self.bias, self.stride,
                         self.padding, self.dilation, self.groups)
+        # ------
 
-
-
-# assume each filter learns a different affine transform
-# assume transfer learning from 2D to 3D kernel
-# TTN = temporal transformer network
-# out_channels = number of filters
-# class ConvTTN3d(conv._ConvNd):
-#
-#     def __init__(self, in_channels, out_channels, kernel_size,
-#                  stride=1, padding=0, dilation=1, groups=1, bias=True, padding_mode='zeros'):
-#         kernel_size = _triple(kernel_size)
-#         stride = _triple(stride)
-#         padding = _triple(padding)
-#         dilation = _triple(dilation)
-#
-#         super(ConvTTN3d, self).__init__(
-#             in_channels, out_channels, kernel_size, stride, padding, dilation,
-#             False, _triple(0), groups, bias, padding_mode)
-#
-#         # ----
-#         # implement for scale, translate, rotate
-#         # ----
-#         self.time_grids = []
-#         # TODO: what if kernel size is int?
-#         for time_point in range(kernel_size[0] - 1):  # time dimension minus 1, keep first frame fixed
-#             scale = torch.nn.Parameter(np.random.random(in_channels))
-#             rotate = torch.nn.Parameter(np.random.random(in_channels))  # counter clockwise
-#             translate_x = torch.nn.Parameter(np.random.random(in_channels))
-#             translate_y = torch.nn.Parameter(np.random.random(in_channels))
-#
-#             self.theta = make_affine_matrix(out_channels, scale, rotate, translate_x, translate_y)
-#             # TODO: figure out size: the target output image size (N×C×H×W). Example: torch.Size((32, 3, 24, 24))
-#             # TODO: how to get height and width of previous kernel
-#             self.grid = affine_grid(theta=self.theta, size=(in_channels, out_channels,))
-#             self.time_grids.append(self.grid)
-#
-#
-#
-#     def forward(self, input, kernels2d):
-#
-#         # ----
-#         self.weight = grid_sample(kernels2d, grid=self.time_grids)
-#         h = F.conv3d(input, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
-#
-#         return h
-#         # ----
-#
-#
-#
-#         # if self.padding_mode == 'circular':
-#         #     expanded_padding = ((self.padding[2] + 1) // 2, self.padding[2] // 2,
-#         #                         (self.padding[1] + 1) // 2, self.padding[1] // 2,
-#         #                         (self.padding[0] + 1) // 2, self.padding[0] // 2)
-#         #     return F.conv3d(F.pad(input, expanded_padding, mode='circular'),
-#         #                     self.weight, self.bias, self.stride, _triple(0),
-#         #                     self.dilation, self.groups)
-#         #
-#         #
-#         #
-#         # return F.conv3d(input, self.weight, self.bias, self.stride,
-#         #                 self.padding, self.dilation, self.groups)
-#
-#
-#
-#
-#
-#
