fdc57eb6a8e12bd67d4632f63327f8a2994c4ff6
u680172 <g.ras@donders.ru.nl>
2019-06-20 18:03:12 +0200

    need to debug 0 1 6 7 and implement 5 and 9

--- omg_emotion/create_moving_mnist.py
+++ omg_emotion/create_moving_mnist.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 from PIL import Image
-import PIL
 
 from relative_baseline.omg_emotion import data_loading as D
 from relative_baseline.omg_emotion.settings import ProjectVariable
@@ -12,6 +11,7 @@ FRAMES = 30
 DTYPE = np.uint8
 RESAMPLE = Image.BILINEAR
 SIDE = 28
+MODE = 'L'
 
 PV = ProjectVariable()
 PV.dataset = 'mnist'
@@ -51,14 +51,20 @@ def move_horizontal(image, frames=FRAMES):
         place = 0
         place_2 = 0
         for i in range(frames // 2, frames):
-            loc_v = last_loc + (place + 1) * velocity_2
-            loc_i = img_x - loc_v
-            if loc_i < 0:
-                video[i, :, (place_2 + 1) * velocity_2:] = image[:, 0:img_x - (place_2 + 1) * velocity_2]
-                place_2 += 1
+            if i == 28:
+                # TODO: properly fix this; make the image move left again
+                video[i] = video[i-2]
+            elif i == 29:
+                video[i] = video[i - 3]
             else:
-                video[i, :, 0:loc_v] = image[:, loc_i:]
-                place += 1
+                loc_v = last_loc + (place + 1) * velocity_2
+                loc_i = img_x - loc_v
+                if loc_i < 0:
+                    video[i, :, (place_2 + 1) * velocity_2:] = image[:, 0:img_x - (place_2 + 1) * velocity_2]
+                    place_2 += 1
+                else:
+                    video[i, :, 0:loc_v] = image[:, loc_i:]
+                    place += 1
 
     return video
 
@@ -80,8 +86,8 @@ def scale(image, frames=FRAMES):
     for i in range(frames // 2):
         side = img_x - i * 2
 
-        if side < 0:
-            side = 0
+        if side <= 0:
+            side = 1
 
         image_resized = image_pil.resize((side, side), RESAMPLE)
         canvas = Image.new(image_pil.mode, size=image_pil.size)
@@ -136,6 +142,9 @@ def scale_up_rotate_clockwise(image, frames=FRAMES):
 
     for i in range(frames):
         side = i * 2
+        if side == 0:
+            side = 1
+
         image_resized = image_pil.resize((side, side), RESAMPLE)
         canvas = Image.new(image_pil.mode, size=image_pil.size)
 
@@ -173,6 +182,7 @@ def move_horizontal_rotate_counter(image, frames=FRAMES):
     velocity_1 = (img_x // 2) // (frames // 2)
     velocity_2 = round(img_x / (frames // 2))
 
+    print('right first: %d' % right_first)
     if right_first:  # 1 == move right
         last_loc = 0
         for i in range(frames // 2):
@@ -199,22 +209,28 @@ def move_horizontal_rotate_counter(image, frames=FRAMES):
         place = 0
         place_2 = 0
         for i in range(frames // 2, frames):
-            image_arr = video[i]
-            loc_v = last_loc + (place + 1) * velocity_2
-            loc_i = img_x - loc_v
-            if loc_i < 0:
-                video[i, :, (place_2 + 1) * velocity_2:] = image_arr[:, 0:img_x - (place_2 + 1) * velocity_2]
-                place_2 += 1
+            if i == 28:
+                # TODO: properly fix this; make the image move left again
+                video[i] = video[i-2]
+            elif i == 29:
+                video[i] = video[i - 3]
             else:
-                video[i, :, 0:loc_v] = image_arr[:, loc_i:]
-                place += 1
+                image_arr = video[i]
+                loc_v = last_loc + (place + 1) * velocity_2
+                loc_i = img_x - loc_v
+                if loc_i < 0:
+                    video[i, :, (place_2 + 1) * velocity_2:] = image_arr[:, 0:img_x - (place_2 + 1) * velocity_2]
+                    place_2 += 1
+                else:
+                    video[i, :, 0:loc_v] = image_arr[:, loc_i:]
+                    place += 1
     
     return video
 
 
 def rotate_clock_and_counter(image, frames=FRAMES):
     video_1 = rotate(direction=-1, image=image, frames=frames//2)
-    video_2 = rotate(direction=-1, image=image, frames=frames//2)
+    video_2 = rotate(direction=1, image=image, frames=frames//2)
 
     img_x, img_y = SIDE, SIDE
     video = np.zeros((frames, img_x, img_y), dtype=DTYPE)
@@ -234,40 +250,76 @@ def random_transformations(image, frames=FRAMES):
 
 
 
-def create_moving_mnist():
+def create_moving_mnist(frames=FRAMES):
     '''
     Method to create the moving MNIST dataset
     Moving MNIST is MNIST but each digit class moves in a specific way, for at least 10 frames
     Affine transformations: rotate, scale, translate x y
-    0	moves only horizontally
-    1	moves only vertically
-    2	scales down and then up
-    3	rotates clockwise
-    4	rotates counter clockwise
+    0	moves only horizontally TODO does not move left (high)
+    1	moves only vertically TODO does not move up (high)
+    2	scales down and then up GOOD
+    3	rotates clockwise GOOD
+    4	rotates counter clockwise GOOD
     TODO 5	moves in circle
-    6	scale up while rotating clockwise
-    7	moves horizontally while rotating counter clockwise
-    8	rotate clockwise and then counter clockwise
+    6	scale up while rotating clockwise TODO: try rotate and then resize (low priority)
+    7	moves horizontally while rotating counter clockwise TODO: doesn't move horizontal (high)
+    8	rotate clockwise and then counter clockwise GOOD
     TODO: 9	random movements
     '''
 
-    mov_mnist_data_folder = os.path.join(PP.moving_mnist_location, 'data')
+    mov_mnist_data_folder = os.path.join(PP.moving_mnist_location, 'debugging_data')
     if not os.path.exists(mov_mnist_data_folder):
         os.mkdir(mov_mnist_data_folder)
 
     # get original mnist
     mnist_all = D.load_mnist(PV)
-    data = mnist_all[1][0]
-    labels = mnist_all[2][0]
-    # change this later
+    data = np.array(mnist_all[1][0])[0:100]
+    labels = np.array(mnist_all[2][0])[0:100]
 
-    # mnist_all[1][0].shape
-    # torch.Size([50000, 1, 28, 28])
-    # mnist_all[2][0].shape
-    # torch.Size([50000])
+    # make data
+    represent = [1, 0, 2, 3, 4, 11, 18, 61]
 
-    # save labels
-    print('asdf')
+    for i in represent:
+        lab = labels[i]
+        print(i, lab)
+        image = data[i][0]
+        video = None
+
+        if lab == 0:
+            video = move_horizontal(image, frames)
+        elif lab == 1:
+            video = move_vertical(image, frames)
+        elif lab == 2:
+            video = scale(image, frames)
+        elif lab == 3:
+            video = rotate(-1, image, frames)
+        elif lab == 4:
+            video = rotate(1, image, frames)
+        elif lab == 5:
+            pass # TODO
+        elif lab == 6:
+            video = scale_up_rotate_clockwise(image, frames)
+        elif lab == 7:
+            video = move_horizontal_rotate_counter(image, frames)
+        elif lab == 8:
+            video = rotate_clock_and_counter(image, frames)
+        elif lab == 9:
+            pass # TODO
+        else:
+            print('Error: lab with value not expected %d' % lab)
+
+        if lab not in [5, 9]:
+            # save arrays as jpgs
+            folder = os.path.join(mov_mnist_data_folder, '%d' % lab)
+            if not os.path.exists(folder):
+                os.mkdir(folder)
+
+            for j in range(FRAMES):
+                im = Image.fromarray(video[j].astype(DTYPE), mode=MODE)
+                path = os.path.join(folder, '%d.jpg' % j)
+                im.save(path)
+
+
 
 
 create_moving_mnist()
