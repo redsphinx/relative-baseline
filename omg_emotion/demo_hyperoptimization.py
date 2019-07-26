f1f06096b072bfad4823e7c6981c9ef0afe7d460
redsphinx <gabi.ras@hotmail.com>
2019-07-27 00:23:46 +0200

    demo hyperoptimization added

---
+++ omg_emotion/demo_hyperoptimization.py
@@ -0,0 +1,81 @@
+from bayes_opt import BayesianOptimization
+from bayes_opt.observer import JSONLogger
+from bayes_opt.event import Events
+
+
+def black_box_function(x, y):
+    """Function with unknown internals we wish to maximize.
+
+    This is just serving as an example, for all intents and
+    purposes think of the internals of this function, i.e.: the process
+    which generates its output values, as unknown.
+    """
+    return -x ** 2 - (y - 1) ** 2 + 1
+
+
+# Bounded region of parameter space
+pbounds = {'x': (2, 4), 'y': (-3, 3)}
+
+optimizer = BayesianOptimization(
+    f=black_box_function,
+    pbounds=pbounds,
+    random_state=1,
+)
+
+
+optimizer.maximize(
+    init_points=2,
+    n_iter=3,
+)
+
+print(optimizer.max)
+
+for i, res in enumerate(optimizer.res):
+    print("Iteration {}: \n\t{}".format(i, res))
+
+optimizer.set_bounds(new_bounds={"x": (-2, 3)})
+
+optimizer.maximize(
+    init_points=0,
+    n_iter=5,
+)
+
+optimizer.probe(
+    params={"x": 0.5, "y": 0.7},
+    lazy=True,
+)
+
+optimizer.probe(
+    params={"x": 0.5, "y": 0.7},
+    lazy=True,
+)
+
+optimizer.probe(
+    params=[-0.3, 0.1],
+    lazy=True,
+)
+
+# Will probe only the two points specified above
+optimizer.maximize(init_points=0, n_iter=0)
+
+logger = JSONLogger(path="./logs.json")
+optimizer.subscribe(Events.OPTMIZATION_STEP, logger)
+
+# Results will be saved in ./logs.json
+optimizer.maximize(
+    init_points=2,
+    n_iter=3,
+)
+
+from bayes_opt.util import load_logs
+
+
+new_optimizer = BayesianOptimization(
+    f=black_box_function,
+    pbounds={"x": (-2, 2), "y": (-2, 2)},
+    verbose=2,
+    random_state=7,
+)
+
+# New optimizer is loaded with previously seen points
+load_logs(new_optimizer, logs=["./logs.json"]);
