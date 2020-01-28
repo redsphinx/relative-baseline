50ff02d42c62de700ca95bc98d35b470ec9fca10
u680172 <g.ras@donders.ru.nl>
2020-01-28 14:21:33 +0100

    running experiment 45-48

--- omg_emotion/dhg_experiments.py
+++ omg_emotion/dhg_experiments.py
@@ -480,7 +480,6 @@ def e36_3D_dhg():
 
     main_file.run(project_variable)
 
-
 # --------------------------------------------------------------------------------------------------------------
 #                       BIGGER MODELS
 # --------------------------------------------------------------------------------------------------------------
@@ -595,16 +594,103 @@ def e44_3D_dhg():
 
     main_file.run(project_variable)
 
+# -------- RERUNNING with adjustment transformation and k0_groups
+
+def e45_3D_dhg():
+    set_init_2()
+    project_variable.experiment_number = 45
+    project_variable.sheet_number = 21
+    project_variable.device = 0
+
+    project_variable.optimizer = 'adam'
+    project_variable.learning_rate = 1e-4
+    project_variable.use_adaptive_lr = False
+    project_variable.num_out_channels = [8, 18]
+    project_variable.transformation_groups = project_variable.num_out_channels
+    project_variable.k0_groups = project_variable.num_out_channels
+
+    main_file.run(project_variable)
+
+def e46_3D_dhg():
+    set_init_2()
+    project_variable.experiment_number = 46
+    project_variable.sheet_number = 21
+    project_variable.device = 0
+
+    project_variable.optimizer = 'adam'
+    project_variable.learning_rate = 1e-4
+    project_variable.use_adaptive_lr = False
+    project_variable.num_out_channels = [12, 22]
+    project_variable.transformation_groups = project_variable.num_out_channels
+    project_variable.k0_groups = project_variable.num_out_channels
+
+    main_file.run(project_variable)
+
+def e47_3D_dhg():
+    set_init_2()
+    project_variable.experiment_number = 47
+    project_variable.sheet_number = 21
+    project_variable.device = 0
+
+    project_variable.optimizer = 'adam'
+    project_variable.learning_rate = 1e-4
+    project_variable.use_adaptive_lr = True
+    project_variable.num_out_channels = [8, 18]
+    project_variable.transformation_groups = project_variable.num_out_channels
+    project_variable.k0_groups = project_variable.num_out_channels
+
+    main_file.run(project_variable)
+
+def e48_3D_dhg():
+    set_init_2()
+    project_variable.experiment_number = 48
+    project_variable.sheet_number = 21
+    project_variable.device = 0
+
+    project_variable.optimizer = 'adam'
+    project_variable.learning_rate = 1e-4
+    project_variable.use_adaptive_lr = True
+    project_variable.num_out_channels = [12, 22]
+    project_variable.transformation_groups = project_variable.num_out_channels
+    project_variable.k0_groups = project_variable.num_out_channels
+
+    main_file.run(project_variable)
+# --------------------------------------------------------------------------------------------------------------
+#                       LESS DATA
+# --------------------------------------------------------------------------------------------------------------
+
+def set_init_3():
+    # project_variable.model_number = 11
+    project_variable.end_epoch = 100
+    project_variable.dataset = 'dhg'
+
+    project_variable.num_in_channels = 1
+    # project_variable.data_points = [140 * 14,  20 * 14, 40 * 14]
+    project_variable.label_size = 14
+    # project_variable.batch_size = 2 * 14
+    project_variable.load_num_frames = 50  # 50
+    project_variable.label_type = 'categories'
+
+    project_variable.repeat_experiments = 10
+    project_variable.save_only_best_run = True
+    project_variable.same_training_data = True
+    project_variable.randomize_training_data = True
+    project_variable.balance_training_data = True
+
+    project_variable.experiment_state = 'new'
+    project_variable.eval_on = 'val'
+
+    project_variable.theta_init = None
+    project_variable.srxy_init = 'eye'
+    project_variable.weight_transform = 'seq'
+
+
 
 project_variable = ProjectVariable(debug_mode=False)
 
 
-# e37_3D_dhg()
-# e38_3D_dhg()
-# e39_3D_dhg()
-# e40_3D_dhg()
-# e41_3D_dhg()
-# e42_3D_dhg()
-# e43_3D_dhg()
-e44_3D_dhg()
+# e45_3D_dhg()
+# e46_3D_dhg()
+# e47_3D_dhg()
+e48_3D_dhg()
 
