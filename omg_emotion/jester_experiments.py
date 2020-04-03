fix new model; colorize gradient method

omg_emotion/jester_experiments.py
@@ -67,8 +67,8 @@ def e_test_3D_jester():
     project_variable.which_methods = ['gradient_method']
     # project_variable.which_layers = ['conv1', 'conv2', 'conv3']
     # project_variable.which_channels = [np.arange(16), np.arange(32), np.arange(32)]
-    project_variable.which_layers = ['conv1', 'conv2']
-    project_variable.which_channels = [np.arange(16), np.arange(32)]
+    project_variable.which_layers = ['conv3']
+    project_variable.which_channels = [np.arange(10)]
 
     main_file.run(project_variable)
 

omg_emotion/models.py
@@ -2543,7 +2543,7 @@ class Experimental_TTN3d_xD(torch.nn.Module):
             if project_variable.num_out_channels == [6, 16, 16]:
                 in_features = 12800
             elif project_variable.num_out_channels == [16, 32, 32]:
-                in_features = 25600
+                in_features = 2560
 
         self.fc1 = torch.nn.Linear(in_features, 120)
         self.fc2 = torch.nn.Linear(120, 84)
@@ -2565,6 +2565,14 @@ class Experimental_TTN3d_xD(torch.nn.Module):
             x, ind2 = self.max_pool_2(x)
         else:
             x = self.max_pool_2(x)
+
+        x = self.conv3(x, device)
+        x = torch.nn.functional.relu(x)
+        
+        if self.return_ind:
+            x, ind3 = self.max_pool_3(x)
+        else:
+            x = self.max_pool_3(x)
 
         _shape = x.shape
         x = x.view(-1, _shape[1] * _shape[2] * _shape[3] * _shape[4])

omg_emotion/xai_tools/layer_visualization.py
@@ -539,11 +539,14 @@ def our_gradient_method(project_variable, data_point, my_model, device):
             data = data_point
             data = torch.nn.Parameter(data, requires_grad=True)
 
-            cntr = 0
+            # cntr = 0
             x_end = None
 
-            # TODO: make it dependant on which layer is chosen to be visualized
-            while cntr < (l + 1):
+            layer_number = int(which_layer[-1])
+
+            # FIX: make it dependant on which layer is chosen to be visualized
+            for cntr in range(layer_number):
+                print('cntr: ', cntr)
                 if cntr == 0:
                     the_input = data
 
@@ -558,7 +561,6 @@ def our_gradient_method(project_variable, data_point, my_model, device):
 
                 x_end = torch.nn.functional.relu(x_max_pool)
                 the_input = x_end
-                cntr = cntr + 1
 
             _, ch, d, h, w = x_end.shape
 