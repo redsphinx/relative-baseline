fixing gradient method in progress

omg_emotion/extract_jester.py
@@ -199,8 +199,10 @@ def standardize_clips(b, e, he, wi, loc):
 
     for vid in tqdm.tqdm(range(b, e)):
         # print('vid = ', vid)
-        vid_path = os.path.join(base_path, all_videos[vid])
-        new_vid_path = os.path.join(new_path, all_videos[vid])
+        # vid_path = os.path.join(base_path, all_videos[vid])
+        # new_vid_path = os.path.join(new_path, all_videos[vid])
+        vid_path = os.path.join(base_path, str(vid))
+        new_vid_path = os.path.join(new_path, str(vid))
         if not os.path.exists(new_vid_path):
             os.mkdir(new_vid_path)
 
@@ -297,23 +299,27 @@ def standardize_clips(b, e, he, wi, loc):
 
 # standardize_clips(120000+1331, 130000, he=224, wi=336, loc='data_224_336') # 1331
 # standardize_clips(130000+1992, 140000, he=224, wi=336, loc='data_224_336') # 1992
-standardize_clips(140000+305, 148092, he=224, wi=336, loc='data_224_336') # 305
-
+# standardize_clips(140000+305, 148092, he=224, wi=336, loc='data_224_336') # 305
+# standardize_clips(101138, 101138+1, he=224, wi=336, loc='data_224_336')
+# standardize_clips(101139, 101139+1, he=224, wi=336, loc='data_224_336')
 
 def triple_check_num_frames_in_folders():
-    path = PP.jester_data_50_75
-    save_path = os.path.join(PP.jester_location, 'missing_frames.txt')
+    # path = PP.jester_data_50_75
+    path = PP.jester_data_224_336
+    # save_path = os.path.join(PP.jester_location, 'missing_frames.txt')
+    save_path = os.path.join(PP.jester_location, 'missing_frames_224x336.txt')
 
     all_folders = os.listdir(path)
     all_folders.sort()
     for i in tqdm.tqdm(range(len(all_folders))):
         p1 = os.path.join(path, all_folders[i])
         num_frames = wc_l(p1)
-        if num_frames < 30:
+        if num_frames != 30:
             print(all_folders[i], num_frames)
             with open(save_path, 'a') as my_file:
                 my_file.write('%s,%d\n' % (all_folders[i], num_frames))
 
+# triple_check_num_frames_in_folders()
 
 def redo_folders_with_few_frames():
     folders_missing_frames = os.path.join(PP.jester_location, 'missing_frames.txt')
@@ -375,29 +381,29 @@ def to_avi(which, b, e):
     print('start and finish: ', b, e)
     assert which in ['test', 'val', 'train']
 
-    if not os.path.exists(PP.jester_data_50_75_avi):
-        os.mkdir(PP.jester_data_50_75_avi)
+    if not os.path.exists(PP.jester_data_224_336_avi):
+        os.mkdir(PP.jester_data_224_336_avi)
 
     labels_path = os.path.join(PP.jester_location, 'labels_%s.npy' % which)
     labels = np.load(labels_path)
     labels = labels[b:e]
 
-    list_files_to_convert = [os.path.join(PP.jester_data_50_75, i) for i in labels[:, 0]]
+    list_files_to_convert = [os.path.join(PP.jester_data_224_336, i) for i in labels[:, 0]]
 
     for file_path in tqdm.tqdm(list_files_to_convert):
 
         fix_file_names_in_folder(file_path)
 
-        dest = os.path.join(PP.jester_data_50_75_avi, '%s.avi' % (file_path.split('/')[-1]))
+        dest = os.path.join(PP.jester_data_224_336_avi, '%s.avi' % (file_path.split('/')[-1]))
 
         jpg_to_avi(file_path, dest)
 
 
-# to_avi('val', 0, 100)
-# to_avi('val', 100, 10000)  # 7393
+# to_avi('val', 0, 3)
+# to_avi('val', 3, 10000)  # 7393
 
 # to_avi('train', 0, 10000)
-# to_avi('train', 10000, 20000)
+# to_avi('train', 10000+2069, 20000) # 2069
 # to_avi('train', 20000, 30000)
 # to_avi('train', 30000, 40000)
 # to_avi('train', 40000, 50000)

omg_emotion/jester_experiments.py
@@ -364,12 +364,40 @@ def e10_conv3DTTN_jester():
     main_file.run(project_variable)
 
 
-project_variable = ProjectVariable(debug_mode=False)
-# project_variable = ProjectVariable(debug_mode=True)
+def vis_conv3DTTN_jester():
+    set_init_1()
+    project_variable.model_number = 20
+    project_variable.experiment_number = 1011010010010
+    # TODO setup for visualization
+    project_variable.sheet_number = 22
+    project_variable.device = 0
+    project_variable.end_epoch = 1
+    project_variable.repeat_experiments = 1
+    project_variable.batch_size = 1 * 27
+
+    project_variable.data_points = [0 * 27, 1 * 27, 0 * 27]
+
+    project_variable.load_model = [10, 20, 2, 0]  # exp, model, epoch, run
+    project_variable.inference_only_mode = True
+    project_variable.do_xai = True
+    project_variable.which_methods = ['gradient_method']
+    project_variable.which_layers = ['conv1']
+    project_variable.which_channels = [np.array([0, 1])]
+
+    project_variable.optimizer = 'adam'
+    project_variable.learning_rate = 0.00005
+    project_variable.use_adaptive_lr = True
+    project_variable.num_out_channels = [0]
+
+    main_file.run(project_variable)
+
+# project_variable = ProjectVariable(debug_mode=False)
+project_variable = ProjectVariable(debug_mode=True)
 
 # e6_conv3DTTN_jester()
 # e7_conv3DTTN_jester()
 # e8_conv3DTTN_jester()
 # eRESNET_jester()
 # e9_conv3DTTN_jester()
-e10_conv3DTTN_jester()
+# e10_conv3DTTN_jester()
+vis_conv3DTTN_jester()

omg_emotion/model_resnet18.py
@@ -111,31 +111,229 @@ class ResNet18(torch.nn.Module):
         self.fc = torch.nn.Linear(512, 27)
 
     def forward(self, x, device):
-        print(x.shape)
         h = self.conv1_relu(x, device)
-        print(h.shape)
         h = self.maxpool(h)
-        print(h.shape)
         h = self.res2a_relu(h, device)
-        print(h.shape)
         h = self.res2b_relu(h, device)
-        print(h.shape)
         h = self.res3a_relu(h, device)
-        print(h.shape)
         h = self.res3b_relu(h, device)
-        print(h.shape)
         h = self.res4a_relu(h, device)
-        print(h.shape)
         h = self.res4b_relu(h, device)
-        print(h.shape)
         h = self.res5a_relu(h, device)
-        print(h.shape)
         h = self.res5b_relu(h, device)
-        print(h.shape)
         h = self.avgpool(h)
-        print(h.shape)
         _shape = h.shape
         h = h.view(-1, _shape[1] * _shape[2] * _shape[3] * _shape[4])
         y = self.fc(h)
-        print(h.shape)
+        return y
+
+
+class ResNet18Explicit(torch.nn.Module):
+    def __init__(self, pv):
+        super(ResNet18Explicit, self).__init__()
+        # self.conv1_relu = ConvolutionBlock(3, 64, pv)
+        self.conv1 = ConvTTN3d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3, project_variable=pv, bias=False)
+        self.bn1 = BatchNorm3d(64)
+        
+        self.maxpool = MaxPool3d(kernel_size=3, padding=1, stride=2, dilation=1)
+        
+        # self.res2a_relu = ResidualBlock(64, 64, pv)
+        self.conv2 = ConvTTN3d(in_channels=64, out_channels=64, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn2 = BatchNorm3d(64)
+        self.conv3 = ConvTTN3d(in_channels=64, out_channels=64, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn3 = BatchNorm3d(64)
+        
+        # self.res2b_relu = ResidualBlock(64, 64, pv)
+        self.conv4 = ConvTTN3d(in_channels=64, out_channels=64, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn4 = BatchNorm3d(64)
+        self.conv5 = ConvTTN3d(in_channels=64, out_channels=64, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn5 = BatchNorm3d(64)
+        
+        # self.res3a_relu = ResidualBlockB(64, 128, pv)
+        self.conv6 = Conv3d(in_channels=64, out_channels=128, kernel_size=1, stride=2, bias=False)
+        self.bn6 = BatchNorm3d(128)
+        self.conv7 = ConvTTN3d(in_channels=64, out_channels=128, kernel_size=3, stride=2, padding=1, project_variable=pv, bias=False)
+        self.bn7 = BatchNorm3d(128)
+        self.conv8 = ConvTTN3d(in_channels=128, out_channels=128, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn8 = BatchNorm3d(128)
+        
+        # self.res3b_relu = ResidualBlock(128, 128, pv)
+        self.conv9 = ConvTTN3d(in_channels=128, out_channels=128, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn9 = BatchNorm3d(128)
+        self.conv10 = ConvTTN3d(in_channels=128, out_channels=128, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn10 = BatchNorm3d(128)
+        
+        # self.res4a_relu = ResidualBlockB(128, 256, pv)
+        self.conv11 = Conv3d(in_channels=128, out_channels=256, kernel_size=1, stride=2, bias=False)
+        self.bn11 = BatchNorm3d(256)
+        self.conv12 = ConvTTN3d(in_channels=128, out_channels=256, kernel_size=3, stride=2, padding=1, project_variable=pv, bias=False)
+        self.bn12 = BatchNorm3d(256)
+        self.conv13 = ConvTTN3d(in_channels=256, out_channels=256, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn13 = BatchNorm3d(256)
+        
+        # self.res4b_relu = ResidualBlock(256, 256, pv)
+        self.conv14 = ConvTTN3d(in_channels=256, out_channels=256, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn14 = BatchNorm3d(256)
+        self.conv15 = ConvTTN3d(in_channels=256, out_channels=256, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn15 = BatchNorm3d(256)
+        
+        # self.res5a_relu = ResidualBlockB(256, 512, pv)
+        self.conv16 = Conv3d(in_channels=256, out_channels=512, kernel_size=1, stride=2, bias=False)
+        self.bn16 = BatchNorm3d(512)
+        self.conv17 = ConvTTN3d(in_channels=256, out_channels=512, kernel_size=3, stride=2, padding=1, project_variable=pv, bias=False)
+        self.bn17 = BatchNorm3d(512)
+        self.conv18 = ConvTTN3d(in_channels=512, out_channels=512, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn18 = BatchNorm3d(512)
+        
+        # self.res5b_relu = ResidualBlock(512, 512, pv)
+        self.conv19 = ConvTTN3d(in_channels=512, out_channels=512, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn19 = BatchNorm3d(512)
+        self.conv20 = ConvTTN3d(in_channels=512, out_channels=512, kernel_size=3, padding=1, project_variable=pv, bias=False)
+        self.bn20 = BatchNorm3d(512)
+        
+        self.avgpool = AdaptiveAvgPool3d(output_size=1)
+        self.fc = torch.nn.Linear(512, 27)
+
+    def forward(self, x, device, stop_at=None):
+        # h = self.conv1_relu(x, device)
+
+        num = 1
+        h = self.conv1(x, device)
+        if stop_at == num:
+            return h
+        h = self.bn1(h)
+        h = relu(h)
+        
+        h = self.maxpool(h)
+        
+        # h = self.res2a_relu(h, device)
+        num = 2
+        h1 = self.conv2(h, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn2(h1)
+        h1 = relu(h1)
+        num = 3
+        h1 = self.conv3(h1, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn3(h1)
+        h = h1 + h
+        h = relu(h)
+        
+        # h = self.res2b_relu(h, device)
+        num = 4
+        h1 = self.conv4(h, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn4(h1)
+        h1 = relu(h1)
+        num = 5
+        h1 = self.conv5(h1, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn5(h1)
+        h = h1 + h
+        h = relu(h)
+        
+        # h = self.res3a_relu(h, device)
+        temp = self.conv6(h)
+        temp = self.bn6(temp)
+        num = 7
+        h1 = self.conv7(h, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn7(h1)
+        h1 = relu(h1)
+        num = 8
+        h1 = self.conv8(h1, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn8(h1)
+        h = temp + h1
+        h = relu(h)
+        
+        # h = self.res3b_relu(h, device)
+        num = 9
+        h1 = self.conv9(h, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn9(h1)
+        h1 = relu(h1)
+        num = 10
+        h1 = self.conv10(h1, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn10(h1)
+        h = h1 + h
+        h = relu(h)
+        
+        # h = self.res4a_relu(h, device)
+        temp = self.conv11(h)
+        temp = self.bn11(temp)
+        num = 12
+        h1 = self.conv12(h, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn12(h1)
+        h1 = relu(h1)
+        num = 13
+        h1 = self.conv13(h1, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn13(h1)
+        h = temp + h1
+        h = relu(h)
+        
+        # h = self.res4b_relu(h, device)
+        num = 14
+        h1 = self.conv14(h, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn14(h1)
+        h1 = relu(h1)
+        num = 15
+        h1 = self.conv15(h1, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn15(h1)
+        h = h1 + h
+        h = relu(h)
+        
+        # h = self.res5a_relu(h, device)
+        temp = self.conv16(h)
+        temp = self.bn16(temp)
+        num = 17
+        h1 = self.conv17(h, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn17(h1)
+        h1 = relu(h1)
+        num = 18
+        h1 = self.conv18(h1, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn18(h1)
+        h = temp + h1
+        h = relu(h)
+        
+        # h = self.res5b_relu(h, device)
+        num = 19
+        h1 = self.conv19(h, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn19(h1)
+        h1 = relu(h1)
+        num = 20
+        h1 = self.conv20(h1, device)
+        if stop_at == num:
+            return h1
+        h1 = self.bn20(h1)
+        h = h1 + h
+        h = relu(h)
+        
+        h = self.avgpool(h)
+        _shape = h.shape
+        h = h.view(-1, _shape[1] * _shape[2] * _shape[3] * _shape[4])
+        y = self.fc(h)
         return y

omg_emotion/setup.py
@@ -187,7 +187,9 @@ def get_model(project_variable):
 
     elif project_variable.model_number == 20:
         model = ResNet18(project_variable)
-        if project_variable.load_model is not None:
+        if type(project_variable.load_model) != bool and not project_variable.load_model is None:
+            model.load_state_dict(torch.load(path, map_location=torch.device('cpu')))
+        elif project_variable.load_model:
             # load resnet18 from pytorch
             tmp_resnet18 = resnet18(pretrained=True)
             # copy the weights
@@ -212,24 +214,24 @@ def get_model(project_variable):
             model.res5b_relu.res_branch2a.first_weight = torch.nn.Parameter(tmp_resnet18.layer4[1].conv1.weight.unsqueeze(2))
             model.res5b_relu.res_branch2b.first_weight = torch.nn.Parameter(tmp_resnet18.layer4[1].conv2.weight.unsqueeze(2))
 
-            # set weights of 3D conv to not require grad
-            model.conv1_relu.conv.weight.requires_grad = False
-            model.res2a_relu.res_branch2a.weight.requires_grad = False
-            model.res2a_relu.res_branch2b.weight.requires_grad = False
-            model.res2b_relu.res_branch2a.weight.requires_grad = False
-            model.res2b_relu.res_branch2b.weight.requires_grad = False
-            model.res3a_relu.res_branch2a.weight.requires_grad = False
-            model.res3a_relu.res_branch2b.weight.requires_grad = False
-            model.res3b_relu.res_branch2a.weight.requires_grad = False
-            model.res3b_relu.res_branch2b.weight.requires_grad = False
-            model.res4a_relu.res_branch2a.weight.requires_grad = False
-            model.res4a_relu.res_branch2b.weight.requires_grad = False
-            model.res4b_relu.res_branch2a.weight.requires_grad = False
-            model.res4b_relu.res_branch2b.weight.requires_grad = False
-            model.res5a_relu.res_branch2a.weight.requires_grad = False
-            model.res5a_relu.res_branch2b.weight.requires_grad = False
-            model.res5b_relu.res_branch2a.weight.requires_grad = False
-            model.res5b_relu.res_branch2b.weight.requires_grad = False
+        # set weights of 3D conv to not require grad
+        model.conv1_relu.conv.weight.requires_grad = False
+        model.res2a_relu.res_branch2a.weight.requires_grad = False
+        model.res2a_relu.res_branch2b.weight.requires_grad = False
+        model.res2b_relu.res_branch2a.weight.requires_grad = False
+        model.res2b_relu.res_branch2b.weight.requires_grad = False
+        model.res3a_relu.res_branch2a.weight.requires_grad = False
+        model.res3a_relu.res_branch2b.weight.requires_grad = False
+        model.res3b_relu.res_branch2a.weight.requires_grad = False
+        model.res3b_relu.res_branch2b.weight.requires_grad = False
+        model.res4a_relu.res_branch2a.weight.requires_grad = False
+        model.res4a_relu.res_branch2b.weight.requires_grad = False
+        model.res4b_relu.res_branch2a.weight.requires_grad = False
+        model.res4b_relu.res_branch2b.weight.requires_grad = False
+        model.res5a_relu.res_branch2a.weight.requires_grad = False
+        model.res5a_relu.res_branch2b.weight.requires_grad = False
+        model.res5b_relu.res_branch2a.weight.requires_grad = False
+        model.res5b_relu.res_branch2b.weight.requires_grad = False
 
     else:
         print('ERROR: model_number=%d not supported' % project_variable.model_number)

omg_emotion/xai_tools/layer_visualization.py
@@ -764,9 +764,212 @@ def our_gradient_method_no_srxy(project_variable, data_point, my_model, device):
     return the_data, all_outputs, None
 
 
-def gradient_method(project_variable, data_point, my_model, device, mode):
+def visualize_resnet18(project_variable, data_point, my_model, device, kernel_visualizations=True, srxy_plots=True):
+    # it plots and saves the first 10 channels of each layer in 3x3 and 7x7 conv
+    assert project_variable.model_number == 20
+    num_channels = 10
 
-    if mode == 'srxy':
+    if kernel_visualizations:
+
+        all_outputs = []
+        data = None
+        all_srxy_params = []
+        # Todo: remove later
+        trafo_per_filter = project_variable.transformations_per_filter
+
+        # get all the layers and the names
+        conv_layers = [i+1 for i in range(20) if i not in [6, 11, 16]]
+
+        for ind in conv_layers:
+            for ch in range(num_channels):
+                data = data_point.copy()
+                data = torch.nn.Parameter(data, requires_grad=True)
+
+                feature_map = my_model(data, device, stop_at=ind)
+
+                _, ch, d, h, w = feature_map.shape
+
+                highest_value = 0
+                ind_1, ind_2, ind_3 = 0, 0, 0
+                for l in range(d):
+                    for m in range(h):
+                        for n in range(w):
+                            val = float(feature_map[0, ch, l, m, n].data.cpu())
+                            if val > highest_value:
+                                highest_value = val
+                                ind_1 = d
+                                ind_2 = m
+                                ind_3 = n
+
+                feature_map[0, ch, ind_1, ind_2, ind_3].backward()
+                image_grad = data.grad
+                final = image_grad[0, :] * data[0, :]
+                # TODO: which frame to select?
+
+                all_finals = []
+                all_finals.append(final)
+
+                # get transformations
+
+                for trafo in range(trafo_per_filter):
+                    s = getattr(getattr(my_model, which_layer), 'scale')[trafo, which_channel]
+                    r = getattr(getattr(my_model, which_layer), 'rotate')[trafo, which_channel]
+                    x = getattr(getattr(my_model, which_layer), 'translate_x')[trafo, which_channel]
+                    y = getattr(getattr(my_model, which_layer), 'translate_y')[trafo, which_channel]
+
+                    # apply them on the final image
+                    next_final = create_next_frame(s, r, x, y, all_finals[trafo], device)
+                    all_finals.append(next_final)
+
+                    # translate parameters to interpretable things
+                    # scale -> 1/s
+                    # rotation -> degrees counterclockwise
+                    # x, y -> half of size image
+
+                    srxy_params.append([1 / float(s), -1 * float(r), -0.5 * float(x), 0.5 * float(y)])
+
+                channels.append(all_finals)
+            srxy_params = np.reshape(srxy_params, (len(project_variable.which_channels[l]), trafo_per_filter, 4))
+            all_srxy_params.append(srxy_params)
+
+            all_outputs.append(channels)
+
+
+        for l in range(len(project_variable.which_layers)):
+            channels = []
+            srxy_params = []
+
+            for c in range(len(project_variable.which_channels[l])):
+                which_layer = project_variable.which_layers[l]
+                which_channel = project_variable.which_channels[l][c]
+
+                data = data_point
+                data = torch.nn.Parameter(data, requires_grad=True)
+
+                # cntr = 0
+                x_end = None
+
+                layer_number = int(which_layer[-1])
+
+                for cntr in range(layer_number):
+                    # print('cntr: ', cntr)
+                    if cntr == 0:
+                        the_input = data
+
+                    x_conv = getattr(my_model, 'conv%d' % (cntr + 1))
+                    x_conv = x_conv(the_input, device)
+
+                    x_max_pool = getattr(my_model, 'max_pool_%d' % (cntr + 1))
+                    if project_variable.return_ind:
+                        x_max_pool, _ = x_max_pool(x_conv)
+                    else:
+                        x_max_pool = x_max_pool(x_conv)
+
+                    x_end = torch.nn.functional.relu(x_max_pool)
+                    the_input = x_end
+
+                _, ch, d, h, w = x_end.shape
+
+                highest_value = 0
+                ind_1, ind_2 = 0, 0
+                for m in range(h):
+                    for n in range(w):
+                        val = float(x_end[0, which_channel, 0, m, n].data.cpu())
+                        if val > highest_value:
+                            highest_value = val
+                            ind_1 = m
+                            ind_2 = n
+                            # TODO also get the frame
+
+                x_end[0, which_channel, 0, ind_1, ind_2].backward()
+
+                image_grad = data.grad
+
+                # FIX color
+                # TODO get the specific high activation frame
+                if project_variable.dataset == 'jester':
+                    final = image_grad[0, :, 0] * data[0, :, 0]
+                else:
+                    final = image_grad[0, 0, 0] * data[0, 0, 0]
+
+                all_finals = []
+                all_finals.append(final)
+
+                # get transformations
+
+                for trafo in range(trafo_per_filter):
+                    s = getattr(getattr(my_model, which_layer), 'scale')[trafo, which_channel]
+                    r = getattr(getattr(my_model, which_layer), 'rotate')[trafo, which_channel]
+                    x = getattr(getattr(my_model, which_layer), 'translate_x')[trafo, which_channel]
+                    y = getattr(getattr(my_model, which_layer), 'translate_y')[trafo, which_channel]
+
+                    # apply them on the final image
+                    next_final = create_next_frame(s, r, x, y, all_finals[trafo], device)
+                    all_finals.append(next_final)
+
+                    # translate parameters to interpretable things
+                    # scale -> 1/s
+                    # rotation -> degrees counterclockwise
+                    # x, y -> half of size image
+
+                    srxy_params.append([1 / float(s), -1 * float(r), -0.5 * float(x), 0.5 * float(y)])
+
+                channels.append(all_finals)
+            srxy_params = np.reshape(srxy_params, (len(project_variable.which_channels[l]), trafo_per_filter, 4))
+            all_srxy_params.append(srxy_params)
+
+            all_outputs.append(channels)
+
+        if project_variable.dataset == 'jester':
+            data = data[0, :, 0]
+        else:
+            data = data[0, 0, 0]
+            data = data.unsqueeze(0)
+
+        data = np.array(data.data.cpu(), dtype=np.uint8)
+
+        processed_outputs = []
+
+        for l in range(len(project_variable.which_layers)):
+            channels = []
+
+            for c in range(len(project_variable.which_channels[l])):
+                all_finals = []
+
+                for t in range(trafo_per_filter + 1):
+
+                    processed_final = all_outputs[l][c][t]
+                    # TODO: finis debugging the coloring
+                    # save as jpg
+                    # transpose channels
+                    # no normalization
+                    # normalization per channel
+
+                    # TODO: turn on normalization again once the network starts training better
+                    # processed_final = normalize(processed_final)
+                    processed_final = processed_final.unsqueeze(0)
+                    processed_final = np.array(processed_final.data.cpu(), dtype=np.uint8)
+
+                    all_finals.append(processed_final)
+
+                channels.append(all_finals)
+
+            processed_outputs.append(channels)
+
+        all_outputs = processed_outputs
+
+        # processed_final = whiten_bg(processed_final)
+        # processed_final = np.expand_dims(processed_final, 0)
+
+        return data, all_outputs, all_srxy_params
+
+
+
+def gradient_method(project_variable, data_point, my_model, device, mode):
+    if project_variable.model_number == 20:
+        return visualize_resnet18(project_variable, data_point, my_model, device)
+
+    elif mode == 'srxy':
         return our_gradient_method(project_variable, data_point, my_model, device)
     else:
         return our_gradient_method_no_srxy(project_variable, data_point, my_model, device)